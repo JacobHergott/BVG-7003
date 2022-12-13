9375a47cbb352bf9159b079f7ef745b94509c4b3
JacobHergott <120519862+JacobHergott@users.noreply.github.com>
2022-12-13 14:01:23 -0800

    Delete Variant Calling directory

--- Variant Calling/scripts/cutad_for.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ../'GBS data'
-ADAP=AGATCGGAA
-exec &> cutadapt.log
-for i in *.fq;
-	do
-		cutadapt -a $ADAP -o $i.fastq $i
-	done
-
--- Variant Calling/scripts/sabre.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DATA=/path/to/fastq
-BARCODE=/path/to/barcode
-TOOL=path/to/sabre
-exec &> sabre.log
-
-sabre se -f FC20150701_1.fq.gz -b FC20150701_1.txt -u unk.fastq
